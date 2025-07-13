1de6a15b57afd922887aea649f0390deec789314
IT25102943 <it25102943@my.sliit.lk>
2025-07-13 21:57:09 +0530

    Add files via upload

---
+++ IT25102943Lab2Q2.java
@@ -0,0 +1,11 @@
+public class IT25102943Lab2Q2
+{  public static void main("String[]args")
+     {
+	  double hypotenhuse ;
+	  double A = 3;
+	  double B = 4;
+	  hypotenhuse = Math.sqrt((Math.pow(A, 2))+(Math.pow(B, 2)));
+	  
+	  System.out.print("hypotenhuse of the right triangle:"+ hypotenhuse");
+	 }
+}
