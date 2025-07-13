0bfd71a324c1cbe493576f20b31be80f15d6a0a2
IT25102943 <it25102943@my.sliit.lk>
2025-07-13 23:10:48 +0530

    Add files via upload

---
+++ IT25102943Lab2Q3.java
@@ -0,0 +1,10 @@
+public class IT25102943Lab2Q3{
+    public static void main(String[]args)
+	{double perimeter = 40;
+	double radius;
+	double PI = 3.14;
+	radius = perimeter / ( 2 * PI );
+	System.out.println("radius of the circuler fence:"+radius);
+	}
+}
+	
